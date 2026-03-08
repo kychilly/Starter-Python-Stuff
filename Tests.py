cec16a00aa783f9acb2d63f6219f002a73c4b5ed
kychilly <randomweebkeys@gmail.com>
2026-03-08 12:08:54 -0400

    test

---
+++ Tests.py
@@ -0,0 +1,28 @@
+#
+# lists = ['me', 'you', 'him', 'her']
+#
+# for i in range(4):
+#     print(lists)
+#
+# while True:
+#     user_input = input('enter an input')
+#     if user_input is not None:
+#         print('goob job')
+#         break
+#
+
+number = input('provide a number')
+
+try:
+    print(number)
+except:
+    print('thats not a number')
+
+
+def say_hi(name):
+    print('Hi, ' + name)
+
+def useless():
+    pass
+
+say_hi('kevin')
--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 toilet_type = 'ohio'
 skibidi = 'toilet'
+list = [1,2,3,4,5,6,7]
+integer = 67
+second_integer = 2
+age = 1;
 
-print(toilet_type, skibidi)
+print(toilet_type, skibidi + ' hi')
+print(list)
+print(str(integer) + str(7))
+print(integer**second_integer)
+
+if age > 21:
+    print('You are old')
+elif age > 18:
+    print('getting old')
+else:
+    print('u r child lol')
