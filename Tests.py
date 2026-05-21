e64ebd86f6a34e590b4f75221a1cf51fb82e7e59
kychilly <randomweebkeys@gmail.com>
2026-05-21 09:18:00 -0400

    lists junk

--- Tests.py
+++ Tests.py
@@ -29,6 +29,47 @@
 #
 # say_hi('kevin')
 
-aList = ["1"]
-item = aList[0]
-print(item)
+# # Lists
+# aList = ["3"]
+# item = aList[-1]
+# print(item)
+#
+# if "1" in aList:
+#     print("1")
+# else:
+#     print("2")
+#
+# aList.append("4")
+# print(aList)
+# aList.insert(2, "5")
+# print(aList)
+#
+# aList.pop() #returns/removes last element
+# print(aList)
+#
+# aList.reverse()
+# aList.sort()
+# bList = sorted(aList)
+#
+# print(aList)
+# aList.clear()
+#
+# cList = [0,1] * 5
+# print(cList)
+# dList = [2,3,4,5]
+# print(cList + dList)
+# eList = dList[1:4] # first index is included, last index is excluded
+# print(eList)
+# fList = dList[::3]
+# gList = dList[::-3]
+# print(fList)
+# print(gList)
+# hList = ["a", "b", "c", "d"]
+# hList2 = hList
+# hList.append("e")
+# print(hList2)
+#
+# iList = [1,2,3,4,5,6,7,8]
+# jList = [i*i for i in iList]
+# print(iList)
+# print(jList)
